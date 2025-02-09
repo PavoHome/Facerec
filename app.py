2d2b750d104f95333f386829ee7470ce83046434
PavoHome <daniel.sjons@gmail.com>
2025-02-09 01:17:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,6 @@ def generate_frames():
             # Yield frame in byte format
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
 @app.route('/')
 def index():
     return render_template('index.html')
